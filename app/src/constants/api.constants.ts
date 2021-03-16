1249aa454bd897544a4c3cc8e4ad5001fcfa07c1
oleksandr melnychenko <oleksandr.melnychenko@live.com>
2021-03-16 14:17:42 +0200

    changed url

--- app/src/constants/api.constants.ts
+++ app/src/constants/api.constants.ts
@@ -1,6 +1,6 @@
 export class API {
-   // public static readonly SERVER_URL = 'http://78.152.175.67:15017/'
-    public static readonly SERVER_URL = ' http://localhost:5000/'
+    public static readonly SERVER_URL = 'http://78.152.175.67:15017/'
+   // public static readonly SERVER_URL = ' http://localhost:5000/'
 
     public static readonly API_PART = 'api/v1/'
 
@@ -16,55 +16,6 @@ export class API {
         UPDATE_USER_PROFILE: API.API_PART + 'identity/profile/update',
         CHANGE_PASSWORD: API.API_PART + 'identity/change/password',
     }
-
-
-    public static readonly GameEndpoints: any = {
-       GET_ALL_COMPANIES: API.API_PART + 'company/get/all',
-       GET_COMPANY_BY_ID: API.API_PART + 'company/get/company?companyNetId=',
-       GET_USER_IN_COMPANY: API.API_PART + 'company/get/in/user',
-       NEW_COMPANY: API.API_PART + 'company/new',
-       UPDATE_COMPANY: API.API_PART + 'company/update',
-    }
-
-    //public static readonly ProjectEndpoints: any = {
-    //    GET_ALL_PROJECTS: API.API_PART + 'project/get/all',
-    //    GET_PROJECT_BY_ID: API.API_PART + 'project/get?netId=',
-    //    GET_BUILDING_TYPES: API.API_PART + 'project/get/building/types',
-    //    GET_CONTRACTOR_TYPES: API.API_PART + 'project/get/contractor/types',
-    //    NEW_PROJECT: API.API_PART + 'project/new',
-    //    GET_PROJECT_BY_COMPANY_ID: API.API_PART + 'project/get/company/id?netId=',
-    //    GET_PROJECTS_BY_USER_NET_ID: API.API_PART + 'project/get/user/id?netId=',
-    //    UPDATE_PROJECT: API.API_PART + 'project/update',
-    //}
-
-    //public static readonly TaskEndpoints: any = {
-    //    GET_ALL_TASKS: API.API_PART + 'task/get/all',
-    //    GET_TASK_SUB_TASKS: API.API_PART + 'task/get/subtasks',
-    //    GET_ALL_TASKS_USER_ID_PROJECT_ID: API.API_PART + 'task/get/all',
-    //    GET_ALL_GANTT_TASKS_USER_ID_PROJECT_ID: API.API_PART + 'task/get/all/gantt',
-    //    GET_ALL_TASKS_GANTT_TEMPLATE: API.API_PART + 'task/get/all/gantt',
-    //    GET_TASK_FILES_BY_TASK_ID: API.API_PART + 'task/get/files',
-    //    GET_TASK_STATUSES: API.API_PART + 'task/get/statuses',
-    //    GET_TASK_LINKED: API.API_PART + 'task/get/linked',
-    //    GET_TASK_COMMENTS: API.API_PART + 'task/get/comments',
-    //    UPDATE_COMMENT: API.API_PART + 'task/update/comment',
-    //    GET_USERS_IN_TASKS: API.API_PART + 'task/get/users?projectNetId=',
-    //    SEARCH_TASKS_BY_PROJECT_ID: API.API_PART + 'task/get/search',
-    //    GET_TASK_BY_NET_ID: API.API_PART + 'task/get',
-    //    GET_CHILDREN_TASK_BY_NET_ID: API.API_PART + 'task/get/children/gantt?netId=',
-    //    GET_TASK_BY_ID: API.API_PART + 'task/get/id',
-    //    NEW_TASK: API.API_PART + 'task/new',
-    //    NEW_TASK_LINKER: API.API_PART + 'task/new/linker',
-    //    NEW_TASK_COMMENT: API.API_PART + 'task/new/task/comment',
-    //    GET_ALL_TASKS_BY_USER_NET_ID_PROJECT_NET_ID: API.API_PART + 'task/get/all/project/user?projectNetId=',
-    //    GET_ALL_TASKS_BY_USER_NETID: API.API_PART + 'task/get/all/user?userNetId=',
-    //    GET_APPROVED_TASKS: API.API_PART + 'task/get/all/approved?offset=',
-    //    GET_UNAPPROVED_TASKS: API.API_PART + 'task/get/all/unapproved',
-    //    UPDATE_TASK: API.API_PART + 'task/update',
-    //    APPROVE_TASK_STATUS_CHANGES: API.API_PART + 'task/approve?requestNetId=',
-    //    UNLINK_TASK: API.API_PART + 'task/unlink?linkerNetUId=',
-    //    CHANGE_ORDER_NUMBER_TASK: API.API_PART + 'task/update/orders',
-    //}
 }
 
 export default API;
